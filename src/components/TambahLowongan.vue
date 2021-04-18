5013b7f369b08f3a282b15833c85a3e7f4fedbe3
Rafa Putri Ayasha <rafaputri86@gmail.com>
2021-04-18 20:40:03 +0700

    coba lagi

--- src/components/TambahLowongan.vue
+++ src/components/TambahLowongan.vue
@@ -1,6 +1,10 @@
 <template>
+<div class ="container-fluid" style="margin: 88px,350px,350px">
+    <div class="container">
+        <div class="card-header bg-info text-center">
+            <h3>Tambah Lowongan</h3>
+        </div>
     <form> 
-       <div>
        <div class="form-group">
            <label class="col-sm-2 col-form-label"> Divisi* </label>
            <select>
@@ -10,6 +14,7 @@
        </div>
        <div class="form-group">
            <label class="col-sm-2 col-form-label"> Posisi* </label>
+           <input placeholder="Posisi">
        </div> 
        <div class="form-group">
            <label class="col-sm-2 col-form-label"> Jenis Lowongan* </label>
@@ -20,16 +25,29 @@
        </div>
         <div class="form-group">
            <label class="col-sm-2 col-form-label"> Jumlah Dibutuhkan* </label>
-       </div>
+            <input placeholder=" 0x Orang">
+    </div>
         <div class="form-group">
            <label class="col-sm-2 col-form-label"> Waktu Pengerjaan* </label>
+           <!-- <div class="col-sm-10"> -->
+           <input placeholder=" 0x Hari">
+           <!-- </div> -->
        </div>
-       </div>
+       <div class="form-group">
+           <label class="col-sm-2 col-form-label"> Kualifikasi* </label>
+    </div>
+     <div class="form-group">
+           <label class="col-sm-2 col-form-label"> Tugas </label>
+    </div>
+    <button type="button" class="btn btn-success btn-md">Simpan</button>
+    <button type="button" class="btn btn-danger btn-md"> Batal </button>
     </form>
+</div>
+</div>
 </template>
 <script>
 export default {
-   name: 'TambahLowongan'
+   name: 'tambah-lowongan'
 }
 //post method atau get
 </script>
