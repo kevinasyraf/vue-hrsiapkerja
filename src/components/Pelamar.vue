c5830a9dfc51dabbcc165b324a02a25d51cea8bc
Anatasya Dwijayanti <anatasyadc@gmail.com>
2021-04-22 00:30:16 +0700

    add detail pelamar

---
+++ src/components/Pelamar.vue
@@ -0,0 +1,45 @@
+<template>
+<div class ="container-fluid" style="margin: 88px,350px,350px">
+    <div class="container">
+        <div class="card mt-3 mx-auto w-50">
+            <div class="card-header bg-info text-center">
+                Detail Pelamar
+            </div>
+            <div class="card-body">
+                    <div class="row">
+                        <div class="col">Nama</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Email</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Nomor Telepon</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Divisi</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Posisi</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Kesesuaian</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Status Pelamar</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">CV</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Tugas 1</div>
+                    </div><br>
+                    <div class="row">
+                        <div class="col">Tugas 2</div>
+                    </div><br>
+                    <button type="button" class="btn btn-primary float-end">Kembali</button>
+                    <button type="button" class="btn btn-success float-end mr-1">Simpan</button>
+                </div>
+            </div>
+    </div>
+</div>
+</template>
