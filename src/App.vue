f6a0e67e130feef5c9cd0124fa78e0370a9b7e4b
Rafa Putri Ayasha <rafaputri86@gmail.com>
2021-04-23 14:12:54 +0700

    ngubah navbar

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,16 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">Features</a>
-          <a class="nav-item nav-link" href="#">Pricing</a>
-          <a class="nav-item nav-link disabled" href="#">Disabled</a>
+          <!-- <a class="nav-item nav-link active" href="#">Lowongan<span class="sr-only">(current)</span></a> -->
+          <a class="nav-item nav-link" href="#">Lowongan</a>
+          <a class="nav-item nav-link" href="#">Lamaran</a>
+          <!-- <a class="nav-item nav-link disabled" href="#">Disabled</a> -->
         </div>
+      </div>
+      <div class="navbar-nav mr-auto">
+        <a class="navbar-brand mb-0" href="#">Nama - Divisi</a>
+        <button type="button" class="btn btn-danger btn-md">Keluar</button>
+
       </div>
     </nav>
     
@@ -30,11 +35,13 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  font-family: Nunito;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #7a7a7a;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
