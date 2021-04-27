f3ebdc4ebd7e21f8f4527cda834c4d6548f804bb
Rafa Putri Ayasha <rafaputri86@gmail.com>
2021-04-28 00:02:35 +0700

    notifikasi popup confirm

--- src/components/TambahPelamar.vue
+++ src/components/TambahPelamar.vue
@@ -35,8 +35,50 @@
            <input type="file" name="filetoupload" class="form-group"/>
            <!-- <input type="text" class="form-group" name="Tugas"> -->
     </form></div>
-    <button type="button" class="btn btn-danger float-end" style="margin-right: 52px">Batal</button>
-    <button @click="saveLowongan" class="btn btn-success float-end mr-1" data-toggle="modal" data-target="#exampleModal">Simpan</button>
+    <!-- <button type="button" class="btn btn-danger float-end" style="margin-right: 52px">Batal</button> -->
+        <a class="btn btn-danger float-end" style="margin-right: 52px"  href="/pelamar" role="button">Batal</a>
+    <!-- <button @click="saveLowongan" class="btn btn-success float-end mr-1" data-toggle="modal" data-target="#exampleModal">Simpan</button> -->
+    <a class="btn btn-success float-end" style="margin-right: 10px" role="button" data-toggle="modal" data-target="#exampleModalCenter">Simpan</a>
+
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                      <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header text-center" v-bind:style="{ backgroundColor: color}">
+                            <h5 class="modal-title w-100" id="exampleModalLongTitle" style="font-size:30px">Confirmation Page</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body" style="font-size:20px">
+                            <p> Apakah Anda yakin akan menambahkan data pelamar baru tersebut? </p>
+                          </div>
+                          <div class="modal-footer">
+                            <a role="button" class="btn btn-success" @click="savePelamar" href="/pelamar">Ya</a>
+                            <!-- <a class="btn btn-success float-end" style="margin-right: 10px" role="button" data-toggle="modal" data-target="#exampleModalCenter2">Ya</a>
+
+                              <div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered" role="document">
+                                  <div class="modal-content">
+                                    <div class="modal-header text-center" v-bind:style="{ backgroundColor: color1}">
+                                      <h5 class="modal-title w-100" id="exampleModalLongTitle" style="font-size:30px">Success Page</h5>
+                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                      </button>
+                                    </div>
+                                    <div class="modal-body"  style="font-size:20px">
+                                      <p class="text-center"> Data Anda Berhasil Diinput </p>
+                                    </div>
+                                    <div class="modal-footer">
+                                      <a role="button" class="btn btn-success btn-block" @click="savePelamar" href="/pelamar">OK</a>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div> -->
+                            <a role="button" class="btn btn-secondary btn-danger" data-dismiss="modal">Tidak</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
     <!-- </div></div> -->
     </form>
 </div></div>
@@ -71,6 +113,8 @@ export default {
        return{
            listKesesuaian:[],
             isSubmitted :false,
+            color : '#3C77BF',
+            color1: '#2ECC71',
            paket: {
                nama : "",
                email : "",
@@ -91,7 +135,7 @@ export default {
       });
   },
   methods: {
-    saveLowongan(e){
+    savePelamar(e){
         axios 
         .post("http://localhost:4000/api/pelamars",this.paket) 
         .then((resp) => {
@@ -131,5 +175,26 @@ button {
   display: inline-block;
   margin: 32px 4px;
   }
+   .btn {
+     padding: 4px 20px !important ;
+    font-size: 18px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    margin: 32px 4px;
+}
+html, body {
+    font-family: 'Nunito', sans-serif;
+}
+.card-header {
+    color: #fff;
+    font-weight: 500;
+    font-size : 20px;
+}
+.modal-title{
+    color: #fff;
+    font-weight: 500;
+    font-size : 20px;
+}
 
 </style>
