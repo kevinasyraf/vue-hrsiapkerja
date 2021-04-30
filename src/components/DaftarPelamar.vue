625c8ab643372ff17fca6e9987ee9a68818af94d
kevinasyraf <kevinasyrafputra@gmail.com>
2021-04-30 13:02:26 +0700

    style: fix daftar pelamar

--- src/components/DaftarPelamar.vue
+++ src/components/DaftarPelamar.vue
@@ -52,10 +52,11 @@
                         <th>Nama</th>
                         <th>Email</th>
                         <th>No. Telepon</th>
-                        <th>Divisi</th>
+                        <!-- <th>Divisi</th>
                         <th>Posisi</th>
-                        <th>Kesesuaian</th>
+                        <th>Kesesuaian</th> -->
                         <th>Status Pelamar</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -66,20 +67,20 @@
                         <!-- <td>{{item.jumlahLowongan}}</td>
                         <td>{{item.tugas}}</td>
                         <td>{{item.status}}</td> -->
-                        <td></td>
-                        <td></td>
-                        <td></td>
                         <td>
                             <a v-if="item.idStatus==1 || item.idStatus==3 || item.idStatus==4" :href="'/pelamar/' + item.id">{{status[item.idStatus - 1]}}</a>
-                            <a v-if="item.idStatus==2" href="" data-toggle="modal" data-target="#interview">Interview</a>
+                            <a v-if="item.idStatus==2" href="" data-toggle="modal" data-target="#interviewModal">Interview</a>
                             <a v-if="item.idStatus==5" href="" data-toggle="modal" data-target="#exampleModalCenter">Negosiasi</a>
                             <a v-if="item.idStatus==6 || item.idStatus==7">{{status[item.idStatus - 1]}}</a>
+                        </td>
+                        <td>
+                            <a class="btn btn-success" :href="'/pelamar/' + item.id" type="button">Detail</a>
                         </td>
                     </tr>
                 </tbody>
             </table>
             
-            <div class="modal fade" id="interview" tabindex="-1" role="dialog" aria-labelledby="interviewTitle" aria-hidden="true">
+            <div class="modal fade" id="interviewModal" tabindex="-1" role="dialog" aria-labelledby="interviewTitle" aria-hidden="true">
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header text-center" v-bind:style="{ backgroundColor: color}">
@@ -93,7 +94,9 @@
                                 <div class="row">
                                     <div class="col">Waktu Interview</div>
                                     <div class="col">
-                                        <input class="form-control" type="datetime-local" value="" id="example-datetime-local-input" v-model="waktuInterview">
+                                        <!-- <input class="form-control" type="text" v-model="idPelamar" readonly> -->
+                                        <input class="form-control" type="datetime-local" value="" id="example-datetime-local-input" v-model="waktuInterviewPelamar">
+                                        <p>{{waktuInterview}}</p>
                                     </div>
                                 </div>
                             </form>
@@ -148,6 +151,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 // eslint-disable-next-line no-unused-vars
 import PelamarDataService from "../services/PelamarDataService";
 
@@ -162,7 +166,8 @@ export default {
             pelamarStatus: '',
             status: ["Screening", "Interviewed", "Additional Assignment", "Negotiation", "Rejected", "Declined", "Hired"],
             divisi: [],
-            waktuInterview: ""
+            waktuInterview: "",
+            currentIdPelamar: -1,
         };
     },
     methods: {
@@ -206,7 +211,8 @@ export default {
                idStatus: this.pelamarStatus,
                waktuInterview: this.waktuInterview
              };
-             PelamarDataService.update(this.pelamar.id, data)
+             console.log(data)
+             PelamarDataService.update(data)
                .then(response => {
                  console.log(response.data);
                  this.message = 'The pelamar was updated successfully!';
@@ -216,12 +222,28 @@ export default {
                });
         },
 
+        showModalInterview(id) {
+            this.currentIdPelamar = id;
+            this.$root.$emit("bv::show::modal", "#interviewModal");
+            // ('#interviewModal').modal('show');
+        },
+
+        updateJadwalInterview(){
+            axios.put('api/pelamar/{this.currentIdPelamar}',
+            {
+                waktuInterview: this.waktuInterviewPelamar
+            })
+        }
+
     },
     mounted() {
         this.retrievePelamar();
         // this.retrieveDivisi();
         this.updateStatus();
-    }
+        this.currentIdPelamar = this.idPelamar;
+        this.waktuInterview = this.waktuInterviewPelamar;
+    },
+    props: ['idPelamar', 'waktuInterviewPelamar']
 };
 </script>
 
