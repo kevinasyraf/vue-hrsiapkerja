c630e444e10ea6bb0c4206e5d7ad114dffe4a955
kevinasyraf <kevinasyrafputra@gmail.com>
2021-06-05 11:55:56 +0700

    refactor: fix login url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/auth/';
+const API_URL = 'https://polar-mountain-10123.herokuapp.com/api/auth/';
 
 class AuthService {
   login(user) {
